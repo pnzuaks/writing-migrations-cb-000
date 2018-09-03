6afa183cb517cde2d45f6384d875050d18953ea3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-03 17:45:45 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class Add_grade_and_birthdate_to_students < ActiveRecord::Migration
   def change
     add_column :artists, :favorite_food, :string
   end
