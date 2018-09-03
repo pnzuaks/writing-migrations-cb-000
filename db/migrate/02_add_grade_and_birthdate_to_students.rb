85fd6d0749adb71072a69cd727beaa40027e05af
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-03 17:47:52 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :grade, :integer, :birthdate, :string
+    add_column :grade, :integer
+    add_column :birthdate, :string
   end
 end
